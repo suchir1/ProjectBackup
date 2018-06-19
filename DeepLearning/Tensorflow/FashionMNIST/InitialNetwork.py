3c92774d32483f0c994859641e04e70ce3e852da
Suchir Bhatt <suchir.bhatt@gmail.com>
2018-06-18 23:20:55 -0400

    Data reading working

--- DeepLearning/Tensorflow/FashionMNIST/InitialNetwork.py
+++ DeepLearning/Tensorflow/FashionMNIST/InitialNetwork.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import mnist_reader
+import os
+import sys
 
-x_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
-x_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
+filepath = sys.argv[0][:sys.argv[0].rfind("/")]+"/data/fashion"
+x_train, y_train = mnist_reader.load_mnist(filepath, kind='train')
+x_test, y_test = mnist_reader.load_mnist(filepath, kind='t10k')
 
