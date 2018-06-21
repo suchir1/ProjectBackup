d0d1796c5f57ceb501da9ad299c79c46c14f63e0
Suchir Bhatt <suchir.bhatt@gmail.com>
2018-06-20 23:37:26 -0400

    Spectral hyperparameter optimization

--- DeepLearning/Tensorflow/FashionMNIST/InitialNetwork.py
+++ DeepLearning/Tensorflow/FashionMNIST/InitialNetwork.py
@@ -16,6 +16,7 @@ x_test, y_test = mnist_reader.load_mnist(filepath, kind='t10k')
 x_train = x_train.reshape((60000,28,28,1))
 x_test = x_test.reshape((10000,28,28,1))
 y_train = tf.keras.utils.to_categorical(y_train, num_classes=num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
 
 model = None
 tf.reset_default_graph()
@@ -23,21 +24,33 @@ tf.reset_default_graph()
 sess = tf.InteractiveSession()
 
 model = tf.keras.Sequential()
-model.add(Conv2D(32,kernel_size=(5,5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(.1))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(.1))
+
 model.add(Flatten())
-model.add(Dense(1000, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
 
 optimizer = Adam(lr=.0000001)
 
 model.compile(optimizer=optimizer,
             loss='categorical_crossentropy',
             metrics=['accuracy'])
-model.fit(x=x_train, y=y_train, batch_size=32, epochs=int(100))
-print(model.output_shape)
+model.fit(x=x_train, y=y_train, batch_size=32, epochs=int(1))
 
 
 score = model.evaluate(x_test, y_test, batch_size=128)
